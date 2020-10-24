7783c751ad12e1bf27c9ca4eb3729618724bfde4
Danny <dlungor@fieldcomplete.com>
2020-10-24 15:39:45 +0300

    Level setup, joystick created

---
+++ Assets/Scripts/UI/StickController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StickController : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+    private Image background;
+    [SerializeField] private Image pointer;
+    private Vector2 direction;
+
+    private void Start()
+    {
+        background = GetComponent<Image>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 position;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(background.rectTransform, eventData.position, eventData.pressEventCamera, out position))
+        {
+            position.x = (position.x / background.rectTransform.sizeDelta.x);
+            position.y = (position.y / background.rectTransform.sizeDelta.y);
+            direction = new Vector2(position.x * 2 - 1, position.y * 2 - 1);
+            direction = direction.magnitude > 1.0f ? direction.normalized : direction;
+
+            pointer.rectTransform.anchoredPosition = new Vector2(direction.x * (background.rectTransform.sizeDelta.x / 2), direction.y * (background.rectTransform.sizeDelta.y / 2));
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        direction = Vector2.zero;
+        pointer.rectTransform.anchoredPosition = Vector2.zero;
+    }
+}
